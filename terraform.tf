af2436920027905d33adaf4cdbeae0fff114c8aa
Josh King <skierx@gmail.com>
2020-12-25 13:42:05 -0500

    Whitespace

--- terraform.tf
+++ terraform.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket   = "terraform.bostondsa.org"
-    key      = "facebook-gcal-sync.tfstate"
-    region   = "us-east-1"
+    bucket = "terraform.bostondsa.org"
+    key    = "facebook-gcal-sync.tfstate"
+    region = "us-east-1"
   }
 }
 
